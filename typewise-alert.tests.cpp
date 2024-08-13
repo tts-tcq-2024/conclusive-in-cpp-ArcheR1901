91c5b43197a1122e4f6b8dccda7a3e012e3ad70b
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-08-13 11:56:20 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,31 +2,27 @@
 #include "typewise-alert.h"
 
 TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
-    EXPECT_EQ(inferBreach(12, 20, 30), TOO_LOW);
-    EXPECT_EQ(inferBreach(25, 20, 30), NORMAL);
-    EXPECT_EQ(inferBreach(35, 20, 30), TOO_HIGH);
+    EXPECT_EQ(inferBreach(20, 10, 30), NORMAL);
+    EXPECT_EQ(inferBreach(5, 10, 30), TOO_LOW);
+    EXPECT_EQ(inferBreach(35, 10, 30), TOO_HIGH);
 }
 
-TEST(TypeWiseAlertTestSuite, ClassifyTemperatureBreach) {
-    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 30), NORMAL);
-    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 40), TOO_HIGH);
-    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40), NORMAL);
-    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50), TOO_HIGH);
-    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30), NORMAL);
-    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45), TOO_HIGH);
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachCorrectly) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 20), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1), TOO_LOW);
 }
 
-TEST(TypeWiseAlertTestSuite, CheckAndAlert) {
-    BatteryCharacter battery = {HI_ACTIVE_COOLING, "BrandA"};
-    
-    // Redirect stdout to capture print output for testing
+TEST(TypeWiseAlertTestSuite, CheckAndAlertSendsToCorrectTarget) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandA"};
+
     testing::internal::CaptureStdout();
-    checkAndAlert(TO_CONTROLLER, battery, 50);
+    checkAndAlert(TO_CONTROLLER, batteryChar, 50);
     std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "feed : 2\n");
+    EXPECT_EQ(output, "feed : 2\n");  // Assuming 2 represents TOO_HIGH
 
     testing::internal::CaptureStdout();
-    checkAndAlert(TO_EMAIL, battery, 50);
+    checkAndAlert(TO_EMAIL, batteryChar, -5);
     output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too low\n");
 }
