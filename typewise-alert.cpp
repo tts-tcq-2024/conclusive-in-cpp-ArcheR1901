3c42be33e3706b05102d527627ac9bba64884f09
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-08-13 11:56:06 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,56 +1,26 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-// Define limits as constants to avoid magic numbers
-const int PASSIVE_COOLING_UPPER_LIMIT = 35;
-const int HI_ACTIVE_COOLING_UPPER_LIMIT = 45;
-const int MED_ACTIVE_COOLING_UPPER_LIMIT = 40;
+// Configuration table for cooling types and their limits
+const TemperatureLimits limits[] = {
+    {0, 35}, // PASSIVE_COOLING
+    {0, 45}, // HI_ACTIVE_COOLING
+    {0, 40}  // MED_ACTIVE_COOLING
+};
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-    if(value < lowerLimit) {
+    if (value < lowerLimit) {
         return TOO_LOW;
     }
-    if(value > upperLimit) {
+    if (value > upperLimit) {
         return TOO_HIGH;
     }
     return NORMAL;
 }
 
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-    int upperLimit = 0;
-    
-    switch(coolingType) {
-        case PASSIVE_COOLING:
-            upperLimit = PASSIVE_COOLING_UPPER_LIMIT;
-            break;
-        case HI_ACTIVE_COOLING:
-            upperLimit = HI_ACTIVE_COOLING_UPPER_LIMIT;
-            break;
-        case MED_ACTIVE_COOLING:
-            upperLimit = MED_ACTIVE_COOLING_UPPER_LIMIT;
-            break;
-        default:
-            // Handle unexpected cooling type
-            return NORMAL;
-    }
-    
-    return inferBreach(temperatureInC, 0, upperLimit);
-}
-
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
-
-    switch(alertTarget) {
-        case TO_CONTROLLER:
-            sendToController(breachType);
-            break;
-        case TO_EMAIL:
-            sendToEmail(breachType);
-            break;
-        default:
-            // Handle unexpected alert target
-            break;
-    }
+    TemperatureLimits tempLimits = limits[coolingType];
+    return inferBreach(temperatureInC, tempLimits.lowerLimit, tempLimits.upperLimit);
 }
 
 void sendToController(BreachType breachType) {
@@ -59,12 +29,18 @@ void sendToController(BreachType breachType) {
 }
 
 void sendToEmail(BreachType breachType) {
-    const char* recepient = "a.b@c.com";
-    if(breachType != NORMAL) {
-        printf("To: %s\n", recepient);
-        const char* breachMessage = (breachType == TOO_LOW) ? 
-                                     "Hi, the temperature is too low\n" : 
-                                     "Hi, the temperature is too high\n";
-        printf("%s", breachMessage);
+    const char* recipient = "a.b@c.com";
+    if (breachType == TOO_LOW) {
+        printf("To: %s\nHi, the temperature is too low\n", recipient);
+    } else if (breachType == TOO_HIGH) {
+        printf("To: %s\nHi, the temperature is too high\n", recipient);
     }
+}
+
+typedef void (*AlertFunction)(BreachType);
+
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    AlertFunction alertFuncs[] = {sendToController, sendToEmail};
+    alertFuncs[alertTarget](breachType);
 }
