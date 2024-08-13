03219a10a10463621486cad8fda4b6c1b66a55e6
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-08-13 11:31:25 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
+    EXPECT_EQ(inferBreach(20, 10, 30), NORMAL);
+    EXPECT_EQ(inferBreach(5, 10, 30), TOO_LOW);
+    EXPECT_EQ(inferBreach(35, 10, 30), TOO_HIGH);
+}
 
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachCorrectly) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 20), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1), TOO_LOW);
+}
 
+TEST(TypeWiseAlertTestSuite, CheckAndAlertSendsToCorrectTarget) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandA"};
+
+    testing::internal::CaptureStdout();
+    checkAndAlert(TO_CONTROLLER, batteryChar, 50);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "feed : 2\n");  // Assuming 2 represents TOO_HIGH
+
+    testing::internal::CaptureStdout();
+    checkAndAlert(TO_EMAIL, batteryChar, -5);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too low\n");
 }
